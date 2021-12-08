d8934eea06557700afd63731aa7d470e9122e5b7
kim <denoh6804@gmail.com>
2021-12-08 23:57:45 +0300

    Primary button added as a component for better reusable. In the Welcome screen and Log in Screen.

---
+++ app/src/main/java/com/example/androiddevchallenge/components/PrimaryButton.kt
@@ -0,0 +1,27 @@
+package com.example.androiddevchallenge.components
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PrimaryButton(modifier: Modifier, buttonText:String) {
+    Button(
+        modifier = modifier,
+        onClick = {},
+        shape = RoundedCornerShape(size = 24.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = MaterialTheme.colors.primary,
+            contentColor = MaterialTheme.colors.onPrimary
+        )
+    ) {
+        Text(text = buttonText, style = MaterialTheme.typography.button)
+    }
+}
