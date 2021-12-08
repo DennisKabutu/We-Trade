acd0e727119480233ff5126e51122f6cc77f8de2
kim <denoh6804@gmail.com>
2021-12-09 00:00:02 +0300

    Log in Screen added. Dark and Light are functional and all component included.

--- app/src/main/java/com/example/androiddevchallenge/LogInScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/LogInScreen.kt
@@ -1,2 +1,119 @@
 package com.example.androiddevchallenge
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Mail
+import androidx.compose.material.icons.outlined.Password
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.components.CredentialsInputForm
+import com.example.androiddevchallenge.components.PrimaryButton
+import com.example.androiddevchallenge.ui.theme.MyTheme
+import java.util.*
+
+
+// To represent The Whole Log in Screen
+@Composable
+fun LogInScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        WelcomeBackSection()
+        LogInCredentialsInputSection()
+
+    }
+}
+
+//Welcome Back section
+
+@Composable
+fun WelcomeBackSection() {
+    Box(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(id = R.drawable.login_bg),
+            contentDescription = "Log in background",
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            color = MaterialTheme.colors.onBackground,
+            style = MaterialTheme.typography.h1,
+            text = "Welcome\n Back ",
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
+                .paddingFromBaseline(top = 152.dp)
+        )
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun LoginScreenPreview() {
+    MyTheme {
+
+        Column(modifier = Modifier.fillMaxSize()) {
+            WelcomeBackSection()
+            LogInCredentialsInputSection()
+
+        }
+
+    }
+
+}
+
+// CredentialInput form to house the password and email input form
+
+@Composable
+fun LogInCredentialsInputSection() {
+    Column(
+        modifier = Modifier.padding(horizontal = 16.dp)
+    ) {
+        CredentialsInputForm(
+            leadingIcon = Icons.Outlined.Mail,
+            labelText = "Email",
+            modifier = Modifier
+                .padding(top = 40.dp)
+                .fillMaxWidth()
+                .height(56.dp)
+
+        )
+        CredentialsInputForm(
+            leadingIcon = Icons.Outlined.Password,
+            labelText = "Password",
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .fillMaxWidth()
+                .height(56.dp)
+
+        )
+        Spacer(modifier = Modifier.height(height = 16.dp))
+        ButtonLogIn()
+
+    }
+}
+
+//Log in Button
+
+@Composable
+fun ButtonLogIn() {
+    PrimaryButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp), buttonText = "log in".toUpperCase(Locale.getDefault())
+    )
+
+}
